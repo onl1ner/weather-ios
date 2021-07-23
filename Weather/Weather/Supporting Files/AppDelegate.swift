61d948c29d1ac89722f2667d972c9be001be2994
Tamerlan Satualdypov <tsatualdypov@gmail.com>
2021-07-23 11:12:54 +0600

    🚀

---
+++ Weather/Weather/Supporting Files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Tamerlan Satualdypov on 23.07.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
